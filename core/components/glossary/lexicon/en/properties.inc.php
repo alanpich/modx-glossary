9896a8e5d3ab000bb5ada07d26e899e1b463906d
Alan Pich <alan.pich@gmail.com>
2012-09-02 13:08:45 +0100

    Added element properties lexicon

---
+++ core/components/glossary/lexicon/en/properties.inc.php
@@ -0,0 +1,11 @@
+<?php
+$_lang['prop_glossary.outerTpl_desc'] = 'Chunk template for glossary outer wrapper';
+$_lang['prop_glossary.groupTpl_desc'] = 'Chunk template for glossary item groups';
+$_lang['prop_glossary.termTpl_desc'] = 'Chunk template for glossary term items';
+$_lang['prop_glossary.showNav_desc'] = 'Shows a quick-nav bar at the top of the glossary';
+$_lang['prop_glossary.navOuterTpl_desc'] = 'Chunk template for outer nav-bar wrapper';
+$_lang['prop_glossary.navItemTpl_desc'] = 'Chunk template for each item in the nav-bar';
+
+$_lang['prop_glossary.resId_desc'] = 'ID of resource displaying a Glossary snippet';
+$_lang['prop_glossary.tpl_desc']   = 'Chunk template for highlighter replacement';
+
