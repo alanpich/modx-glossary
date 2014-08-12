b8d78e6a90534924a647a39ef907464e6784ec0a
vundicind <vundicind@gmail.com>
2014-08-12 12:29:16 +0300

    Added raw translations to Romanian and Russian languages

---
+++ core/components/glossary/lexicon/ro/default.inc.php
@@ -0,0 +1,17 @@
+<?php
+$_lang['glossary'] = 'Glosar';
+$_lang['glossary.description'] = 'Glosar de termeni';
+$_lang['glossary.management'] = 'Glosar';
+$_lang['glossary.term'] = 'Termen';
+$_lang['glossary.terms'] = 'Termeni';
+$_lang['glossary.explanation'] = 'Explicație';
+$_lang['glossary.search...'] = 'Caută...';
+$_lang['glossary.term_remove'] = 'Șterge termen';
+$_lang['glossary.term_remove_confirm'] = 'Dați clic pe OK pentru a șterge acest termen';
+$_lang['glossary.term_create'] = 'Adugă termen';
+$_lang['glossary.term_create_long'] = 'Adaugă un termen nou în glosar';
+$_lang['glossary.term_err_ns_term'] = 'Termenul nu este valid sau lipsește';
+$_lang['glossary.term_err_ae_term'] = 'Acest termen deja există';
+$_lang['glossary.term_err_ns_explanation'] = 'Explicația nu este validă sau lipsește'; 
+$_lang['glossary.create'] = 'Adaugă';
+$_lang['glossary.cancel'] = 'Renunță';
---
+++ core/components/glossary/lexicon/ru/properties.inc.php
@@ -0,0 +1,11 @@
+<?php
+$_lang['prop_glossary.outerTpl_desc'] = 'Чанк для списка терминов';
+$_lang['prop_glossary.groupTpl_desc'] = 'Чанк для груп терминов';
+$_lang['prop_glossary.termTpl_desc'] = 'Чанк для терминов';
+$_lang['prop_glossary.showNav_desc'] = 'Показать алфавитный указатель';
+$_lang['prop_glossary.navOuterTpl_desc'] = 'Чанк для алфавитного указателя';
+$_lang['prop_glossary.navItemTpl_desc'] = 'Чанк для букв в алфавитном указателе';
+
+$_lang['prop_glossary.resId_desc'] = 'ID страницы на которой находится сниппет Glossary';
+$_lang['prop_glossary.tpl_desc']   = 'Чанк для выделенных терминов';
+
