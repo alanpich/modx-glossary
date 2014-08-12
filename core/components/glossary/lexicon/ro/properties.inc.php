385c7f4c8e8d0abbd27d0d74c0f1a71bf656e39a
vundicind <vundicind@gmail.com>
2014-08-12 18:05:04 +0300

    Fixed some typos

--- core/components/glossary/lexicon/ro/properties.inc.php
+++ core/components/glossary/lexicon/ro/properties.inc.php
@@ -1,7 +1,7 @@
 <?php
 $_lang['prop_glossary.outerTpl_desc'] = 'Chunk-ul pentru lista de termeni';
-$_lang['prop_glossary.groupTpl_desc'] = 'Chunk-ul pentru grup de termeni';
-$_lang['prop_glossary.termTpl_desc'] = 'Chunk-ul pentru termen';
+$_lang['prop_glossary.groupTpl_desc'] = 'Chunk-ul pentru grupurile de termeni';
+$_lang['prop_glossary.termTpl_desc'] = 'Chunk-ul pentru termeni';
 $_lang['prop_glossary.showNav_desc'] = 'Afișează indexul alfabetic';
 $_lang['prop_glossary.navOuterTpl_desc'] = 'Chunk-ul pentru indexul alfabetic';
 $_lang['prop_glossary.navItemTpl_desc'] = 'Chunk-ul pentru literele din index';
--- core/components/glossary/lexicon/ru/properties.inc.php
+++ core/components/glossary/lexicon/ru/properties.inc.php
@@ -1,6 +1,6 @@
 <?php
 $_lang['prop_glossary.outerTpl_desc'] = 'Чанк для списка терминов';
-$_lang['prop_glossary.groupTpl_desc'] = 'Чанк для груп терминов';
+$_lang['prop_glossary.groupTpl_desc'] = 'Чанк для групп терминов';
 $_lang['prop_glossary.termTpl_desc'] = 'Чанк для терминов';
 $_lang['prop_glossary.showNav_desc'] = 'Показать алфавитный указатель';
 $_lang['prop_glossary.navOuterTpl_desc'] = 'Чанк для алфавитного указателя';
